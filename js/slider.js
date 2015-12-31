b4bbb1b0c7336985967543d8e659a8d68e849fdd
kolialike <kolialike@gmail.com>
2015-12-31 11:59:55 +0300

    bislite

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
+
+$(document).ready(function()
+{
+    $('#slider1').tinycarousel();
+});
+    
---
+++ копия/js/slider.js
@@ -0,0 +1,15 @@
+$(window).load(function() {
+  $('.slid .flexslider').flexslider({
+    animation: "slide"
+  });
+});
+
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemWidth: 250,
+    itemMargin: 40,
+  });
+});
+    
